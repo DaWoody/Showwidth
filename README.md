7e9a5fd05588da80faddb65d5b250f717706d14a
DaWoody <wedfelt@kth.se>
2013-07-08 16:10:47 +0200

    Ok did some changes with the styles being pushed through the script, making the pop-up look a little bit better.. some comments added.

--- README.md
+++ README.md
@@ -1,4 +1,18 @@
 Showwidth
 =========
 
-A small jQuery plugin I made to help front-end developers find out their browsers width while developing responsive design
+A small jQuery plugin I made to help front-end developers find out their browsers width while developing responsive design.
+
+How it works: 
+
+By adding the code in a .js file or referencing it from: // Add google code place here :)
+The browser will, if resized, launch a pop-up window that displays the width of the browser.
+
+The code is open source etc. So share and care :) 
+If you have any ideas on how to make this plugin better or want to join me making the code better, please feel free to do so! :)
+
+Otherwise you can follow me on GitHub (https://github.com/DaWoody) or on BitBucket (https://bitbucket.com/DaWoody). Or you can listen to my musical endevours here: http://www.woodfieldtheartist.net.
+
+Cheers!
+
+/Johan Wedfelt
--- jquery.showwidth.js
+++ jquery.showwidth.js
@@ -5,35 +5,55 @@
 *					aimed to help developers understand when their responsive/adaptive layout 
 *					breaks etc.
 *	License: 		This is free through the open license agreement, share, care and spread :). 
-*	Author: 		Johan Wedfelt, check me out on GitHub
+*	Author: 		Johan Wedfelt, check me out on GitHub, https://github.com/DaWoody, or go listen to my music: http://www.woodfieldtheartist.net
+*	
 */
 
 (function( $ ){
 
 	$(window).resize(function(){	
 
-			//If the window isn't already open...
-			var showContainer = window.open('', 'showContainer', 'height=100, width=250, status=no, toolbar=no, location=no, menu=no');
+			/*
+			*	Stating below what I am doing.. this is done every resize as it is now..
+			*/
 
-			//Print out some start markup
+			//If the pop-up isn't already open, open it!
+			var showContainer = window.open('', 'showContainer', 'height=100, width=250, status=no, toolbar=no, location=no, menubar=no, titlebar=no');
+			//Print out some start markup in the pop-up
 			showContainer.document.write('<header><h1 id="widthText"></h1><header>');
-			
-			//Define the h1 element in the new window
+			//Define the h1 element in the pop-up
 			var showContainerH1 = showContainer.document.getElementById("widthText");
-
+			//Get the body tag in the pop-up
+			var showContainerBody = showContainer.document.body;
 			//calculate the width of the original window	
 			var width = $(window).width();
 
 			/*
-			*	Set some styles to the markup of the pop-up
+			*	Set some styles to the markup of the pop-up.
+			*	Defining some variables for background, color etc
+			*	Change these variables below if you want another layout...
 			*/
-			$(showContainerH1).css('color','red');
-			$(showContainerH1).css('text-align','center');
 
+			var backgroundColor = '#ccc';
+			var textColor = 'black';
+			var textShadow = '1px 1px 2px #fff';
+			var fontSize = '38px';
+			var paddingTop = '15px';
+
+			/*
+			*	Setting the changes to the css with some jQuery...
+			*/
+
+			$(showContainerBody).css('background-color', backgroundColor);
+			$(showContainerH1).css('color', textColor);
+			$(showContainerH1).css('font-size', fontSize);
+			$(showContainerH1).css('text-shadow', textShadow);
+			$(showContainerH1).css('text-align','center');
+			$(showContainerH1).css('padding-top', paddingTop);
 
 
 			//Use jQuery to update the h1 element in the pop-up.
-			$(showContainerH1).html('Width: '+ width + 'px');	
+			$(showContainerH1).html('Width: '+ width + 'px');
 
 	});
 
